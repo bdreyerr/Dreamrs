f07e3be4168a82ad4234f4f787f9517b58956a78
bdreyerr <bendreyer21@gmail.com>
2023-10-28 14:08:19 -0700

    Initial Commit

---
+++ Dreamrs/DreamrsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamrsApp.swift
+//  Dreamrs
+//
+//  Created by Ben Dreyer on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DreamrsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
